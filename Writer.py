b6b6acfd8419bf8539028abf20281f5ec0e0d433
Chamindu Weerasinghe <chamindu626@gmail.com>
2021-04-25 19:37:43 +0530

    Update Writer.py

--- Writer.py
+++ Writer.py
@@ -1,23 +1,39 @@
 from threading import Thread
 from os import path
-import pytz
 from datetime import datetime
 
 class Write(Thread):
     def write_file(self, list, filename):
         with open(filename, "a") as file:
             i = 0
+            dataToPrint = ''
             for elemnt in list:
                 i += 1
                 if i == 1:
-                    key = str(elemnt).replace("'", '')
-                    key = str(key).replace("Key.",'')
-                    file.write(key)
+                    key = str(str(elemnt).replace("'", '')).replace("Key.",'')
+                    dataToPrint = key
                 else:
-                    file.write(str(elemnt))
-                if i < 3:
-                    file.write(str(","))
+                    dataToPrint = dataToPrint+','+elemnt
+            file.write(dataToPrint)
             file.write("\n")
+
+class rawfileWriter(Thread):
+    def write_rawfile(self,filename,event,eventType,time,fatigueScore):
+        key = str(str(event).replace("'", '')).replace("Key.", '')
+        dataToWrite = key + ',' + eventType + ',' + str(time) + ',' + fatigueScore
+        if not path.exists(filename):
+            f = open(filename, "w")
+            f.write('key,event_type,time,fatigue_score\n')
+            f.write(dataToWrite+'\n')
+            f.close()
+        else:
+            f = open(filename, "a")
+            f.write(dataToWrite+'\n')
+            f.close()
+
+
+
+
 
 class ScroreWriter():
     @staticmethod
@@ -25,10 +41,10 @@ class ScroreWriter():
         if not path.exists(filLocation):
             f = open(filLocation, "w")
             f.write('Time,Score\n')
-            f.write(str(datetime.now(tz=pytz.utc)) + ',' + score)
+            f.write(str(datetime.now()) + ',' + score)
             f.close()
         else:
             f = open(filLocation, 'a')
             f.write("\n")
-            f.write(str(datetime.now(tz=pytz.utc)) + ',' + score)
+            f.write(str(datetime.now()) + ',' + score)
             f.close()
